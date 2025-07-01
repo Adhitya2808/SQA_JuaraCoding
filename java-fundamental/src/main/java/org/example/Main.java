c10bd5f3a1c292653380c28d5d9c40d1986742d6
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2025-07-01 19:14:43 +0700

    initial

---
+++ java-fundamental/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+class Main
+{
+    int x = 5;
+    public static void main(String args[])
+    {
+        {
+            int x = 10;
+            System.out.println(x);
+
+        }
+
+    }
+
+}
