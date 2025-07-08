fef11a0fcad8ac2be53390dc042ac18801627a48
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2025-07-08 19:06:06 +0700

    refactor: comment

--- Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Logout.java
+++ Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Logout.java
@@ -1,7 +1,7 @@
 package com.Tohsaka.FilmBioskop.Feature;
 
 public class Logout {
-    public static void Keluar(){                    // Method untuk Logout/Keluar
+    public static void Keluar(){                    // Method untuk Logout atau Keluar
         System.out.println("Anda Telah Keluar");
     }
 }
