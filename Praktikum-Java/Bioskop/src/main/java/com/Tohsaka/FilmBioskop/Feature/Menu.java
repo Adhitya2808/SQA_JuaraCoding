d745b1a9ea8290d541e162a2b37dc41d119fa936
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2025-07-10 19:13:49 +0700

    add: feature menu and retry

--- Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Menu.java
+++ Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Menu.java
@@ -1,11 +1,10 @@
 package com.Tohsaka.FilmBioskop.Feature;
-
 import java.util.Scanner;
 
 public class Menu {
-    public static void TampilkanMenu(){                     // Method Untuk Menampilkan Daftar Menu
+    protected static void TampilkanMenu(){                     // Method Untuk Menampilkan Daftar Menu
         System.out.println("=== Menu Utama ===");
-        System.out.println("1. Tampilkan Data Film");
+        System.out.println("1. Tampilkan Daftar Film");
         System.out.println("2. Input Data Film");
         System.out.println("3. Cari Film");
         System.out.println("4. Keluar");
@@ -14,21 +13,21 @@ public class Menu {
 
         switch (menu.nextInt()){                           // Percabangan Untuk Menentukan Menu Apa Yang Dipilih oleh user
             case 1:
-                Film.TampilkanFilm();                      // method Untuk Menampilkan Daftar Film
+                Film.tampilkanfilm();                      // method Untuk Menampilkan Daftar Film
                 break;
             case 2:
-                Film.InputFilm();                          // method Untuk Memasukkan Nama Film
+                Film.inputfilm();                          // method Untuk Memasukkan Nama Film
                 break;
             case 3:
-                Film.CariFilm();                           // method Untuk Mencari Daftar Film
+                Film.carifilm();                           // method Untuk Mencari Daftar Film
                 break;
             case 4:
                 System.out.println("Anda Telah Logout");
-                Logout.Keluar();                          // method Untuk Logout/Keluar
+                Logout.keluar();                          // method Untuk Logout/Keluar
                 break;
             default:
-                System.out.println("Error Input");
-                Retry.Ulang();                           // method Untuk Mengulang Pilihan
+                System.out.println("Input Error");
+                Retry.ulang();                           // method Untuk Mengulang Pilihan
                 break;
         }
     }
--- Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Retry.java
+++ Praktikum-Java/Bioskop/src/main/java/com/Tohsaka/FilmBioskop/Feature/Retry.java
@@ -3,19 +3,19 @@ package com.Tohsaka.FilmBioskop.Feature;
 import java.util.Scanner;
 
 public class Retry {
-    static void Ulang(){                                                            // Method Untuk Mengulang Pilihan
-        String ulang;
+    private static String ulang;
+    protected static void ulang(){                            // Method Untuk Mengulang Pilihan
         System.out.print("Apakah anda ingin Memilih Menu Lain?(ya/tidak) : ");
         Scanner input = new Scanner(System.in);
         ulang = input.nextLine();
 
         if(ulang.equalsIgnoreCase("ya")||ulang.equalsIgnoreCase("iya")){      // Jika User Memilih ya atau iya maka akan kembali ke daftar menu
             Menu.TampilkanMenu();
-        } else if(ulang.equals("tidak")){                                                            // jika user memilih tidak maka akan Logout atau keluar
-            Logout.Keluar();
+        } else if(ulang.equalsIgnoreCase("tidak")){        // jika user memilih tidak maka akan Logout atau keluar
+            Logout.keluar();
         } else{
-            System.out.println("Error Input");             // jika user tidak memilih salah satu maka akan error input dan mengulang hingga inputan benar
-            Ulang();
+            System.out.println("Input Error");             // jika user tidak memilih salah satu maka akan error input dan mengulang hingga inputan benar
+            ulang();
         }
     }
 }
